b8914e80b4be46b0b6135e6f947893214f893332
LikhovidVladislav <vladlihovidych@gmail.com>
2019-01-13 19:34:55 +0200

    Initial Commit

---
+++ schedule/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  schedule
+//
+//  Created by Влад Лиховид on 1/13/19.
+//  Copyright © 2019 Влад Лиховид. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
